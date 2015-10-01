753b06fda0b018c65f560e781fe2db61088b724c
Shadowigor <shadowigorr@gmail.com>
2015-10-01 15:10:06 +0200

    Moved from checksum based comparision to write access time comparision

--- src/lnb_fileindexer.c
+++ src/lnb_fileindexer.c
@@ -8,17 +8,14 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stddef.h>
+#include <time.h>
 #include <sys/stat.h>
 #include <openssl/md5.h>
 
 #define E(x)   {int __tmp_e = (x); if(__tmp_e)return __tmp_e;}
-#define __STR(x) #x
-#define STR(x) __STR(x)
 
 #define LIST_CHUNK_SIZE  1048576
 #define LIST_MAX_CHUNKS  65536
-#define THREAD_COUNT     16
-#define FILE_CHUNK_SIZE  134217728
 
 char **exclude;
 int exclude_len;
@@ -29,7 +26,6 @@ struct list_s
     struct entry_s *entry;
     int offset;
     int nchunk;
-    int current_chunk;
     int len;
     long size;
 } file_list, dir_list, link_list;
@@ -40,7 +36,7 @@ struct entry_s
     int perm;
     int gid;
     int uid;
-    long size;
+    time_t time;
     char path;
 };
 
@@ -54,7 +50,6 @@ int list_init(struct list_s *list)
     list->offset = 0;
     list->len = 0;
     list->size = 0;
-    list->current_chunk = 0;
     list->entry = (struct entry_s*)list->chunks[0];
 
     return 0;
@@ -86,7 +81,6 @@ int list_add(struct list_s *list, char *path, struct entry_s *entry)
     list->entry = (struct entry_s*)(((char*)list->entry) + entry->len + offsetof(struct entry_s, path));
     list->offset += entry->len + offsetof(struct entry_s, path);
     list->len++;
-    list->size += entry->size;
     *((char*)list->entry) = 0; // End of chunk
 
     return 0;
@@ -147,6 +141,8 @@ int list_dir(const char *root_name)
 
         if(root_name[0] == '/' && root_name[1] == '\0')
             entry.len = snprintf(path, PATH_MAX, "/%s", name);
+        else if(root_name[0] == '.' && root_name[1] == '\0')
+            entry.len = snprintf(path, PATH_MAX, "%s", name);
         else
             entry.len = snprintf(path, PATH_MAX, "%s/%s", root_name, name);
 
@@ -159,7 +155,7 @@ int list_dir(const char *root_name)
         entry.gid = file_stat.st_gid;
         entry.uid = file_stat.st_uid;
         entry.perm = file_stat.st_mode & ALLPERMS;
-        entry.size = file_stat.st_size;
+        entry.time = file_stat.st_mtim.tv_sec;
 
         if(S_ISLNK(file_stat.st_mode))
         {
@@ -190,62 +186,12 @@ int list_dir(const char *root_name)
     return 0;
 }
 
-struct thread_arg
-{
-    struct entry_s *entry;
-    int chunk;
-    char *cs;
-    int error;
-    int cnt;
-};
-
-void *checksum_worker(void *varg)
-{
-    struct thread_arg *arg = (struct thread_arg*)varg;
-    unsigned char *data;
-    MD5_CTX md5_context;
-    FILE *file;
-    int len;
-
-    data = malloc(FILE_CHUNK_SIZE);
-    if(data == NULL)
-    {
-        arg->error = errno;
-        return NULL;
-    }
-
-    for(int i = 0; i < arg->cnt; i++)
-    {
-        file = fopen(&arg->entry->path, "r");
-        if(file == NULL)
-        {
-            arg->error = errno;
-            return &arg->entry->path;
-        }
-
-        MD5_Init(&md5_context);
-        while((len = fread(data, 1, FILE_CHUNK_SIZE, file)) != 0)
-            MD5_Update(&md5_context, data, len);
-        MD5_Final(arg->cs, &md5_context);
-        list_next(&file_list, &arg->chunk, arg->entry);
-        arg->cs += MD5_DIGEST_LENGTH;
-        fclose(file);
-    }
-
-    free(data);
-
-    return NULL;
-}
-
 int main(int argc, char **argv)
 {
-    struct thread_arg args[THREAD_COUNT];
     struct entry_s *entry;
-    pthread_t threads[THREAD_COUNT];
     FILE *file;
-    char *cs, *cs_current, *error_file, path[PATH_MAX];
-    int pos, error, i, chunk;
-    long size_per_th, size_next_th, size_current;
+    char path[PATH_MAX];
+    int len, error, i, chunk;
 
     if(argc < 2)
     {
@@ -258,6 +204,12 @@ int main(int argc, char **argv)
     {
         exclude = &argv[2];
         exclude_len = argc - 2;
+    }
+
+    if(chdir(argv[1]))
+    {
+        perror("Error");
+        return EINVAL;
     }
 
     if(list_init(&dir_list))
@@ -286,7 +238,7 @@ int main(int argc, char **argv)
 
     printf("Indexing files... ");
     fflush(stdout);
-    if(list_dir(argv[1]))
+    if(list_dir("."))
     {
         list_del(&dir_list);
         list_del(&file_list);
@@ -322,104 +274,6 @@ int main(int argc, char **argv)
     fclose(file);
 
     printf("Done\n");
-    printf("Listing links... ");
-    fflush(stdout);
-
-    file = fopen("/tmp/lnb_fileindexer_link", "w");
-    if(file == NULL)
-    {
-        error = errno;
-        fprintf(stderr, "Error: Unable to open temp file\n");
-        fflush(stderr);
-        list_del(&dir_list);
-        list_del(&file_list);
-        list_del(&link_list);
-        return error;
-    }
-
-    entry = (struct entry_s *)link_list.chunks[0];
-    chunk = 0;
-    for(i = 0; i < link_list.len; i++)
-    {
-        pos = readlink(&entry->path, path, PATH_MAX);
-        path[pos] = '\0';
-        fprintf(file, "%s\t%d\t%d\t%d\t%s\n", &entry->path, entry->perm, entry->gid, entry->uid, path);
-        entry = list_next(&dir_list, &chunk, entry);
-    }
-
-    printf("Done\n");
-    printf("Calculating checksums with " STR(THREAD_COUNT) " treads... ");
-    fflush(stdout);
-    cs = malloc(file_list.len * 16);
-    if(cs == NULL)
-    {
-        list_del(&dir_list);
-        list_del(&file_list);
-        list_del(&link_list);
-        fprintf(stderr, "Error: Out of memory\n");
-        fflush(stderr);
-        return ENOMEM;
-    }
-
-    pos = 0;
-    entry = (struct entry_s*)file_list.chunks[0];
-    chunk = 0;
-    size_per_th = file_list.size / THREAD_COUNT;
-    size_next_th = 0;
-    size_current = 0;
-    error = 0;
-    cs_current = cs;
-printf("\n");
-    for(i = 0; i < THREAD_COUNT - 1; i++)
-    {
-        args[i].cnt = 0;
-        args[i].error = 0;
-        args[i].entry = entry;
-        args[i].chunk = chunk;
-        args[i].cs = cs_current;
-        size_next_th += size_per_th;
-        do
-        {
-            entry = list_next(&file_list, &chunk, entry);
-            size_current += entry->size;
-            args[i].cnt++;
-            pos++;
-            cs_current += MD5_DIGEST_LENGTH;
-        } while(size_current < size_next_th);
-        pthread_create(&threads[i], NULL, checksum_worker, &args[i]);
-        printf("Thread %2d: %d\n", i, size_current - (size_next_th - size_per_th));
-    }
-    printf("Thread %2d: %d\n", THREAD_COUNT - 1, file_list.len - size_current);
-
-    args[THREAD_COUNT - 1].cnt = file_list.len - pos;
-    args[THREAD_COUNT - 1].entry = entry;
-    args[THREAD_COUNT - 1].chunk = chunk;
-    args[THREAD_COUNT - 1].cs = cs_current;
-    args[THREAD_COUNT - 1].error = 0;
-    pthread_create(&threads[THREAD_COUNT - 1], NULL, checksum_worker, &args[THREAD_COUNT - 1]);
-
-    for(i = 0; i < THREAD_COUNT; i++)
-    {
-            pthread_join(threads[i], (void**)&error_file);
-            error |= args[i].error;
-    }
-
-    if(error)
-    {
-        if(error_file == NULL)
-            fprintf(stderr, "Error: Out of memory\n");
-        else
-            fprintf(stderr, "Error: Cannot access all files (e.g. %s)\n", error_file);
-        fflush(stderr);
-        list_del(&dir_list);
-        list_del(&file_list);
-        list_del(&link_list);
-        return EACCES;
-    }
-
-    printf("Done\n");
-    fflush(stdout);
-
     printf("Listing files... ");
     fflush(stdout);
     file = fopen("/tmp/lnb_fileindexer_files", "w");
@@ -435,20 +289,26 @@ printf("\n");
 
     entry = (struct entry_s*)file_list.chunks[0];
     chunk = 0;
-    cs_current = cs;
     for(i = 0; i < file_list.len; i++)
     {
-        fprintf(file, "%s\t%d\t%d\t%d\t", &entry->path, entry->perm, entry->gid, entry->uid);
-        for(int j = 0; j < MD5_DIGEST_LENGTH; j++)
-        {
-            fprintf(file, "%02hhX", *cs_current);
-            cs_current++;
-        }
-        fprintf(file, "\n");
+        fprintf(file, "%s\t%d\t%d\t%d\t%s", &entry->path, entry->perm, entry->gid, entry->uid, ctime(&entry->time));
         entry = list_next(&file_list, &chunk, entry);
     }
+    printf("Done\n");
+    printf("Listing links... ");
+    fflush(stdout);
+
+    entry = (struct entry_s *)link_list.chunks[0];
+    chunk = 0;
+    for(i = 0; i < link_list.len; i++)
+    {
+        len = readlink(&entry->path, path, PATH_MAX);
+        path[len] = '\0';
+        fprintf(file, "%s\t%d\t%d\t%d\t%s\n", &entry->path, entry->perm, entry->gid, entry->uid, path);
+        entry = list_next(&dir_list, &chunk, entry);
+    }
+
     fclose(file);
-    free(cs);
     list_del(&dir_list);
     list_del(&file_list);
     list_del(&link_list);
